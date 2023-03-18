9e1e0a3c59056fa6490b4d40045a6a0ad60157f7
katarinjah <katarina.d.262@gmail.com>
2023-03-18 13:06:02 +0100

    Completed feature 9

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Header } from './components/Header/Header';
 import { Container } from './components/Container/Container';
+import { Loader } from './components/Loader/Loader'
 import { SearchField } from './components/SearchField/SearchField'
 import { Footer } from './components/Footer/Footer';
 import { UserList } from './components/UserList/UserList';
@@ -11,6 +12,8 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [users, setUsers] = useState([]);
   const [filteredUsers, setFilteredUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [isSearching, setIsSearching] = useState(false);
 
   const toggleLayout = () => {
     localStorage.setItem("layout", layout === "list" ? "grid" : "list");
@@ -24,16 +27,20 @@ function App() {
 
   useEffect(() => {
     const fetchData = async () => {
-        const response = await fetch("https://randomuser.me/api/?results=15");
-        const data = await response.json();
-        setUsers(data.results);
+      setIsLoading(true);
+      const response = await fetch("https://randomuser.me/api/?results=15");
+      const data = await response.json();
+      setUsers(data.results);
+      setIsLoading(false);
     };
     fetchData();
   }, []);
 
   useEffect(() => {
+    setIsSearching(true);
     const newFilteredUsers = users.filter(user => user.name.first.toLowerCase().includes(searchQuery.toLowerCase()) || user.name.last.toLowerCase().includes(searchQuery.toLowerCase()));
     setFilteredUsers(newFilteredUsers);
+    setIsSearching(false);
   }, [searchQuery, users])
 
   useEffect(() => {
@@ -47,11 +54,17 @@ function App() {
     <>
       <Header toggleLayout={toggleLayout} layout={layout} setLayout={setLayout} handleReload={handleReload} />
       <Container>
-        <SearchField searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
-        {layout === "list" ? (
-          <UserList users={filteredUsers} />
+        {isLoading ? (
+          <Loader />
         ) : (
-          <UserGrid users={filteredUsers} />
+          <>
+            {isSearching ? null : <SearchField searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>}
+            {layout === "list" ? (
+              <UserList users={filteredUsers} />
+            ) : (
+              <UserGrid users={filteredUsers} />
+            )}
+          </>
         )}
       </Container>
       <Footer />
